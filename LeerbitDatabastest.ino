16acffb3ce1611b3aa2bf4a5b9429e01dfbbe140
CalfChristiaan <58288475+CalfChristiaan@users.noreply.github.com>
2019-12-12 11:59:03 +0100

    Create LeerbitDatabastest.ino

---
+++ LeerbitDatabastest.ino
@@ -0,0 +1,288 @@
+/*
+   Schooljaar: 2019-2020
+   Opleiding: ICT NSE Leerjaar 1
+   Project: "The Challenge"
+   Porduct: Leerbit
+   Projectgroep: A
+   Klas: D1
+
+   Deelnemers:
+   Marko Estrada Rodriguez 16138643
+   Christiaan Calf 18129390
+   Daniël Bleeker 19046278
+   Lucas Dekker 19038364
+   Eddy Al Bazze 19085834
+*/
+
+
+#include <Arduino.h>
+#include <U8x8lib.h>
+#include <U8g2lib.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+
+
+
+
+
+
+/* Allocation I2C (RTC)
+** SDA - 20
+** SCL - 21
+** Reset - 22
+*/
+
+/*Allocation Oled Display*/
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ 22);   // All Boards without Reset of the Display
+
+
+/* Allocation sd card reader
+   SD card attached to SPI bus on arduino mega as follows:
+ ** MOSI - pin 51
+ ** MISO - pin 50
+ ** CLK - pin 52
+ ** CS - pin 53
+  /*
+
+  //Constants
+
+  /*Allocation Leds*/
+const int led_1_G = 46;
+const int led_1_R = 47;
+const int led_2_G = 44;
+const int led_2_R = 45;
+const int led_3_G = 42;
+const int led_3_R = 43;
+const int led_4_G = 40;
+const int led_4_R = 41;
+const int led_5_G = 38;
+const int led_5_R = 39;
+const int led_6_G = 36;
+const int led_6_R = 37;
+const int led_7_G = 34;
+const int led_7_R = 35;
+const int led_8_G = 32;
+const int led_8_R = 33;
+const int led_9_G = A14;
+const int led_9_R = A15;
+const int led_10_G = A12;
+const int led_10_R = A13;
+const int led_11_G = A10;
+const int led_11_R = A11;
+const int led_12_G = A8;
+const int led_12_R = A9;
+const int led_13_G = A6;
+const int led_13_R = A7;
+const int led_14_G = A4;
+const int led_14_R = A5;
+const int led_15_G = A2;
+const int led_15_R = A3;
+
+
+/*Allocation buttons*/
+const int buttonPinA = 29;
+const int buttonPinB = 11;
+const int buttonPinC = 9;
+const int buttonPinD = 7;
+
+/*Allocation Touch buttons*/
+const int buttonPinLeft = 31;
+const int buttonPinRight = 3;
+
+/*Allocation light Sensor*/
+const int photocellPin = A0;
+
+
+//Variables
+int lightValue;  // variable for storing the value from photoresistor (0-1023)
+
+int buttonState_A = 0;         // variable for reading the pushbutton A status
+int buttonState_B = 0;         // variable for reading the pushbutton B status
+int buttonState_C = 0;         // variable for reading the pushbutton C status
+int buttonState_D = 0;         // variable for reading the pushbutton D status
+
+int buttonState_Left = 0;         // variable for reading the left touchbutton status
+int buttonState_Right = 0;        // variable for reading the right touchbutton status
+int indrukken=0;
+
+
+#define gear_width 30
+#define gear_height 30
+static const unsigned char gear_logo[] U8X8_PROGMEM = {
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x07, 0x00,
+  0x00, 0x1C, 0x0F, 0x00, 0x00, 0xBC, 0x0F, 0x00, 0x00, 0xFC, 0xCF, 0x00,
+  0xE0, 0xFE, 0xFF, 0x01, 0xF0, 0x3F, 0xFF, 0x01, 0xE0, 0x07, 0xF8, 0x00,
+  0xE0, 0x03, 0xF0, 0x00, 0xC0, 0x01, 0xE0, 0x01, 0xE0, 0x00, 0xC0, 0x0F,
+  0xFC, 0x00, 0xC0, 0x0F, 0xFC, 0x00, 0xC0, 0x0F, 0xFC, 0x00, 0x80, 0x03,
+  0xF8, 0x00, 0x80, 0x01, 0xE0, 0x00, 0xC0, 0x03, 0xE0, 0x00, 0xC0, 0x07,
+  0xF0, 0x00, 0xC0, 0x0F, 0xF8, 0x01, 0xE0, 0x07, 0xF8, 0x03, 0xF0, 0x07,
+  0xF0, 0x07, 0x78, 0x00, 0x00, 0x3F, 0x7F, 0x00, 0x00, 0xFF, 0xFF, 0x00,
+  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xC7, 0x63, 0x00, 0x00, 0xC2, 0x03, 0x00,
+  0x00, 0xC0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+
+
+
+
+
+
+
+void setup() {
+  Serial.begin(9600); // Start serial communication
+  u8g2.begin(buttonState_Left,buttonState_Right,buttonState_A);  // Start oled display
+
+  pinMode(led_1_G, OUTPUT); // Declare the LED 1 GREEN as an output
+  pinMode(led_1_R, OUTPUT); // Declare the LED 1 RED as an output
+  pinMode(led_2_G, OUTPUT); // Declare the LED 2 GREEN as an output
+  pinMode(led_2_R, OUTPUT); // Declare the LED 2 RED as an output
+  pinMode(led_3_G, OUTPUT); // Declare the LED 3 GREEN as an output
+  pinMode(led_3_R, OUTPUT); // Declare the LED 3 RED as an output
+  pinMode(led_4_G, OUTPUT); // Declare the LED 4 GREEN as an output
+  pinMode(led_4_R, OUTPUT); // Declare the LED 4 RED as an output
+  pinMode(led_5_G, OUTPUT); // Declare the LED 5 GREEN as an output
+  pinMode(led_5_R, OUTPUT); // Declare the LED 5 RED as an output
+  pinMode(led_6_G, OUTPUT); // Declare the LED 6 GREEN as an output
+  pinMode(led_6_R, OUTPUT); // Declare the LED 6 RED as an output
+  pinMode(led_7_G, OUTPUT); // Declare the LED 7 GREEN as an output
+  pinMode(led_7_R, OUTPUT); // Declare the LED 7 RED as an output
+  pinMode(led_8_G, OUTPUT); // Declare the LED 8 GREEN as an output
+  pinMode(led_8_R, OUTPUT); // Declare the LED 8 RED as an output
+  pinMode(led_9_G, OUTPUT); // Declare the LED 9 GREEN as an output
+  pinMode(led_9_R, OUTPUT); // Declare the LED 9 RED as an output
+  pinMode(led_10_G, OUTPUT); // Declare the LED 10 GREEN as an output
+  pinMode(led_10_R, OUTPUT); // Declare the LED 10 RED as an output
+  pinMode(led_11_G, OUTPUT); // Declare the LED 11 GREEN as an output
+  pinMode(led_11_R, OUTPUT); // Declare the LED 11 RED as an output
+  pinMode(led_12_G, OUTPUT); // Declare the LED 12 GREEN as an output
+  pinMode(led_12_R, OUTPUT); // Declare the LED 12 RED as an output
+  pinMode(led_13_G, OUTPUT); // Declare the LED 13 GREEN as an output
+  pinMode(led_13_R, OUTPUT); // Declare the LED 13 RED as an output
+  pinMode(led_14_G, OUTPUT); // Declare the LED 14 GREEN as an output
+  pinMode(led_14_R, OUTPUT); // Declare the LED 14 RED as an output
+  pinMode(led_15_G, OUTPUT); // Declare the LED 15 GREEN as an output
+  pinMode(led_15_R, OUTPUT); // Declare the LED 15 RED as an output
+
+
+
+  pinMode(buttonPinA, INPUT_PULLUP); // Initialize the pushbutton A pin as an input and enable the internal pull-up resistor
+  pinMode(buttonPinB, INPUT_PULLUP); // Initialize the pushbutton B pin as an input and enable the internal pull-up resistor
+  pinMode(buttonPinC, INPUT_PULLUP); // Initialize the pushbutton C pin as an input and enable the internal pull-up resistor
+  pinMode(buttonPinD, INPUT_PULLUP); // Initialize the pushbutton D pin as an input and enable the internal pull-up resistor
+
+  pinMode(buttonPinLeft, INPUT); // Initialize the Touch button left pin as an input
+  pinMode(buttonPinRight, INPUT); // Initialize the Touch button right pin as an input
+
+
+  pinMode(photocellPin, INPUT); // Initialize the Light sensor pin as an input
+
+
+
+}
+
+void loop() {
+
+
+  lightValue = analogRead(photocellPin);  //Store the value from the Light sensor in the variable
+
+indrukken=u8g2.getMenuEvent();
+Serial.println(buttonState_Left);
+
+
+
+ // init_Screen();
+  Menu();
+
+  
+
+ // Serial.println(lightValue);
+  digitalWrite(led_1_R, HIGH); // Turn the LED 1 GREEN on
+  //digitalWrite(led_1_G, HIGH); // Turn the LED 2 RED on
+  //digitalWrite(led_2_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_2_G, HIGH); // Turn the LED 2 RED on
+  digitalWrite(led_3_R, HIGH); // Turn the LED 1 GREEN on
+  //digitalWrite(led_3_G, HIGH); // Turn the LED 2 RED on
+  //digitalWrite(led_4_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_4_G, HIGH); // Turn the LED 2 RED on
+  // digitalWrite(led_5_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_5_G, HIGH); // Turn the LED 2 RED on
+  digitalWrite(led_6_R, HIGH); // Turn the LED 1 GREEN on
+  //digitalWrite(led_6_G, HIGH); // Turn the LED 2 RED on
+  digitalWrite(led_7_R, HIGH); // Turn the LED 1 GREEN on
+  //digitalWrite(led_7_G, HIGH); // Turn the LED 2 RED on
+  digitalWrite(led_8_R, HIGH); // Turn the LED 1 GREEN on
+  // digitalWrite(led_8_G, HIGH); // Turn the LED 2 RED on
+  // digitalWrite(led_9_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_9_G, HIGH); // Turn the LED 2 RED on
+  // digitalWrite(led_10_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_10_G, HIGH); // Turn the LED 2 RED on
+  // digitalWrite(led_11_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_11_G, HIGH); // Turn the LED 2 RED on
+  digitalWrite(led_12_R, HIGH); // Turn the LED 1 GREEN on
+  // digitalWrite(led_12_G, HIGH); // Turn the LED 2 RED on
+  //  digitalWrite(led_13_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_13_G, HIGH); // Turn the LED 2 RED on
+  //  digitalWrite(led_14_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_14_G, HIGH); // Turn the LED 2 RED on
+  //digitalWrite(led_15_R, HIGH); // Turn the LED 1 GREEN on
+  digitalWrite(led_15_G, HIGH); // Turn the LED 2 RED on
+
+
+ // Serial.println(buttonState_A);
+ // Serial.println(buttonState_B);
+ // Serial.println(buttonState_C);
+ // Serial.println(buttonState_D);
+
+  buttonState_A = digitalRead(buttonPinA);  //Store the state of button A in the variable
+  buttonState_B = digitalRead(buttonPinB);  //Store the state of button B in the variable
+  buttonState_C = digitalRead(buttonPinC);  //Store the state of button C in the variable
+  buttonState_D = digitalRead(buttonPinD);  //Store the state of button D in the variable
+
+  buttonState_Left = digitalRead(buttonPinLeft); //Store the state of left touch button in the variable
+  buttonState_Right = digitalRead(buttonPinRight); //Store the state of right touch button in the variable
+
+
+}
+void init_Screen(){
+u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_t0_22_me); // choose a suitable font
+  u8g2.drawStr(25, 18, "LeerBit"); // write something to the internal memory
+  u8g2.setDrawColor(1);
+  u8g2.setBitmapMode(1 /* solid */);
+  u8g2.drawXBMP(50, 25, gear_width, gear_height, gear_logo);
+
+
+  /*char buf[4];
+    sprintf (buf, "%d", lightValue);
+    u8g2.drawStr(48, 42, buf);
+
+    u8g2.setCursor(0, 35);
+    u8g2.print(buttonState_A);
+    u8g2.setCursor(10, 35);
+    u8g2.print(buttonState_B);
+    u8g2.setCursor(20, 35);
+    u8g2.print(buttonState_C);
+    u8g2.setCursor(30, 35);
+    u8g2.print(buttonState_D);
+  */
+
+
+
+
+  u8g2.sendBuffer();          // transfer internal memory to the display
+
+
+
+}
+void Menu(){
+u8g2.clearBuffer();          // clear the internal memory
+u8g2.setFont(u8g2_font_t0_12_me); // choose a suitable font
+u8g2.userInterfaceSelectionList("Menu", 2, "Vakken\nProfiel");
+u8g2.sendBuffer();          // transfer internal memory to the display
+
+
+
+
+
+}
