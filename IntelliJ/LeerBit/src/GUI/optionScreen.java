adf179f210f33757cb7e8188969dff26c845443c
Lupindakaas <57939116+Lupindakaas@users.noreply.github.com>
2019-12-17 08:40:57 +0100

    ratings scherm is verweiderd, was nutteloos

--- IntelliJ/LeerBit/src/GUI/optionScreen.java
+++ IntelliJ/LeerBit/src/GUI/optionScreen.java
@@ -1,8 +1,15 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class optionScreen {
     private JButton button_persoonsGegevens;
@@ -53,4 +60,137 @@ public class optionScreen {
         });
 
     }
+
+    public static void writeFile() {
+
+        List data_leerling = new List();
+        List data_vragen = new List();
+        List data_score = new List();
+        List data_vak = new List();
+
+        String url = "jdbc:mysql://localhost:3306/leerbit?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true&&serverTimezone=UTC";
+        String username = "root";
+        String password = "";
+        Connection conn = null;
+
+        try {
+            //Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        /* De tabel leerling uitlezen, en dan opzetten in het bestandje leerling.txt */
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("Select voor_naam from leerling");
+
+            while (rs.next()) {
+                //String id = rs.getString("leerling_nummer");
+                String voor_naam = rs.getString("voor_naam");
+                //String achter_naam = rs.getString("achter_naam");
+                //String leeftijd = rs.getString("leeftijd");
+                System.out.println(voor_naam);
+                data_leerling.add(voor_naam);
+
+            }
+            writeToFile((java.util.List) data_leerling, "C:\\Users\\mjnde\\OneDrive\\Documenten\\leerbit\\leerling.txt");
+
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        /* De tabel vragen uitlezen, en dan omzetten in het .txt bestand vragen.txt */
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("Select * from vragen");
+
+            while (rs.next()) {
+                String id = rs.getString("vraag_nummer");
+                String vak_naam = rs.getString("vak_naam");
+                String opdracht = rs.getString("opdracht");
+                String antwoord1 = rs.getString("antwoord1");
+                String antwoord2 = rs.getString("antwoord2");
+                String antwoord3 = rs.getString("antwoord3");
+                String antwoord4 = rs.getString("antwoord4");
+                String juisteAntwoord = rs.getString("juiste_antwoord");
+                System.out.println(id + vak_naam + opdracht + antwoord1 + antwoord2 + antwoord3 + antwoord4 + juisteAntwoord);
+                data_vragen.add(id + "," + vak_naam + "," + opdracht + "," + antwoord1 + "," + antwoord2 + "," + antwoord3 + "," + antwoord4 + "," + juisteAntwoord);
+
+            }
+            writeToFile((java.util.List) data_vragen, "C:\\Users\\mjnde\\OneDrive\\Documenten\\leerbit\\vragen.txt");
+
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("Select * from score");
+
+            while (rs.next()) {
+                String id = rs.getString("leerling_nummer");
+                String vak_naam = rs.getString("vak_naam");
+                String aantal_goed = rs.getString("aantal_goed");
+                System.out.println(id + vak_naam + aantal_goed);
+                data_score.add(id + "," + vak_naam + "," + aantal_goed);
+            }
+            writeToFile((java.util.List) data_score, "C:\\Users\\mjnde\\OneDrive\\Documenten\\leerbit\\score.txt");
+
+            st.close();
+            rs.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("Select * from vak");
+
+            while (rs.next()) {
+                String vak_naam = rs.getString("vak_naam");
+                String aantal_vragen = rs.getString("hoeveelheid_vragen");
+                System.out.println(vak_naam + aantal_vragen);
+                data_vak.add(vak_naam + "," + aantal_vragen);
+            }
+            writeToFile((java.util.List) data_vak, "C:\\Users\\mjnde\\OneDrive\\Documenten\\leerbit\\vak.txt");
+
+            st.close();
+            rs.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+    private static void writeToFile(java.util.List list, String path) {
+        BufferedWriter out = null;
+        try {
+            File leerbit = new File(path);
+            out = new BufferedWriter(new FileWriter(leerbit, true));
+            for (Object s : list) {
+                out.write((String) s);
+                out.newLine();
+            }
+            out.close();
+        } catch (IOException e) {
+        }
+    }
+    public static void enter(String bestand) {
+        List enter = new List();
+        enter.add("\n");
+        String url = "C:\\Users\\mjnde\\OneDrive\\Documenten\\leerbit\\" + bestand + ".txt";
+        writeToFile((java.util.List) enter, url);
+    }
+
 }
