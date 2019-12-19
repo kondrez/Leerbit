19fd45e06492c5c1b1c8df5db25976f779fe43b2
Lupindakaas <57939116+Lupindakaas@users.noreply.github.com>
2019-12-19 09:39:06 +0100

    beetje gecommend

--- IntelliJ/LeerBit/src/GUI/optionScreen.java
+++ IntelliJ/LeerBit/src/GUI/optionScreen.java
@@ -20,56 +20,59 @@ public class optionScreen {
     private JLabel label_message;
     private JButton button_export;
 
-    public optionScreen(JFrame optie) {         // dit is een methode met daarin alle code voor de knoppen. met de JFrame optie als parameter zodat deze gebruikt kan worden door de methode
+    public optionScreen(JFrame optie) {
+        /* dit is een methode met daarin alle code voor de knoppen. */
+
         button_persoonsGegevens.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // deze code word aangeroepen als er op de knop "button_persoonsGegevens" gedrukt word, hij zal de pagina "Peronal Data" openen
+                /* deze code word aangeroepen als er op de knop "button_persoonsGegevens" gedrukt word, hij zal de pagina "Peronal Data" openen */
 
-                JFrame PData = new JFrame("PersoonsDataScherm");          // dit zorgt ervoor dat de programatuur weet dat dit een scherm, of "JFrame" is.
-                PData.setContentPane(new personalDataScreen(PData).panel_PData);    // hier zeg je waaruit die JFrame gaat bestaan, dus je kiest welk panel. let op dat je de panel public maakt in je code.
-                PData.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          // hier word gezegd wat er gebeurt als er op sluiten gedrukt word, in dit geval word het programma afgesloten
-                PData.pack();                                                  // hier worden alle gui elementen uit het bestant "optie" op dezelfde JFrame gezet
-                PData.setVisible(true);                                        // spreekt wel voor zich, hier word de JFrame "optie" zichtbaar gemaakt
+                JFrame PData = new JFrame("PersoonsDataScherm");
+                PData.setContentPane(new personalDataScreen(PData).panel_PData);
+                PData.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                PData.pack();
+                PData.setVisible(true);
 
-                optie.dispose();                                                // hier word het scherm wat je verlaat afgesloten
+                optie.dispose();
             }
         });
         button_opdrachten.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // deze code word aangeroepen als er op de knop "opdrachten" gedrukt word
+                /* deze code word aangeroepen als er op de knop "opdrachten" gedrukt word */
 
-                JFrame exercises = new JFrame("opdrachten scerm");          // dit zorgt ervoor dat de programatuur weet dat dit een scherm, of "JFrame" is.
-                exercises.setContentPane(new exercisesScreen(exercises).panel_opdrachten);    // hier zeg je waaruit die JFrame gaat bestaan, dus je kiest welk panel. let op dat je de panel public maakt in je code.
-                exercises.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          // hier word gezegd wat er gebeurt als er op sluiten gedrukt word, in dit geval word het programma afgesloten
-                exercises.pack();                                                  // hier worden alle gui elementen uit het bestant "optie" op dezelfde JFrame gezet
-                exercises.setVisible(true);                                        // spreekt wel voor zich, hier word de JFrame "optie" zichtbaar gemaakt
-                optie.dispose();                                                   // hier word het scherm wat je verlaat afgesloten
+                JFrame exercises = new JFrame("opdrachten scerm");
+                exercises.setContentPane(new exercisesScreen(exercises).panel_opdrachten);
+                exercises.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                exercises.pack();
+                exercises.setVisible(true);
+                optie.dispose();
             }
         });
         button_scores.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // deze code word aangeroepen als er op de knop "button_scores" gedrukt word
-                JFrame scores = new JFrame("scores scherm");                 // dit zorgt ervoor dat de programatuur weet dat dit een scherm, of "JFrame" is.
-                scores.setContentPane(new scoresScreen(scores).panel_scoresScreen);    // hier zeg je waaruit die JFrame gaat bestaan, dus je kiest welk panel. let op dat je de panel public maakt in je code.
-                scores.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          // hier word gezegd wat er gebeurt als er op sluiten gedrukt word, in dit geval word het programma afgesloten
-                scores.pack();                                                  // hier worden alle gui elementen uit het bestant "optie" op dezelfde JFrame gezet
-                scores.setVisible(true);                                        // spreekt wel voor zich, hier word de JFrame "optie" zichtbaar gemaakt
-                optie.dispose();                                                // hier word het scherm wat je verlaat afgesloten
+                /* deze code word aangeroepen als er op de knop "button_scores" gedrukt word */
+
+                JFrame scores = new JFrame("scores scherm");
+                scores.setContentPane(new scoresScreen(scores).panel_scoresScreen);
+                scores.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                scores.pack();
+                scores.setVisible(true);
+                optie.dispose();
             }
         });
 
         button_export.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                writeFile();
+                writeSD();
             }
         });
     }
 
-    private static void writeFile() {
+    private static void writeSD() {
         /* deze methode leest een database uit, en zet dit daarna in een .txt bestand */
 
         java.util.List<String> data_leerling = new ArrayList<String>();
