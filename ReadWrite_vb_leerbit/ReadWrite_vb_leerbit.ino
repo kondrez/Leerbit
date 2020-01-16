59654f443fd83f33e4e69839c0ecbdde130dd618
Lupindakaas <57939116+Lupindakaas@users.noreply.github.com>
2020-01-16 13:14:01 +0100

    werkend gemaakt

--- ReadWrite_vb_leerbit/ReadWrite_vb_leerbit.ino
+++ ReadWrite_vb_leerbit/ReadWrite_vb_leerbit.ino
@@ -1,27 +1,31 @@
 /*
   SD card read/write
 
- This example shows how to read and write data to and from an SD card file
- The circuit:
- * SD card attached to SPI bus as follows:
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
  ** MOSI - pin 11
  ** MISO - pin 12
  ** CLK - pin 13
  ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
 
- This example code is in the public domain.
+  This example code is in the public domain.
 
- */
+*/
 
 #include <SPI.h>
 #include <SD.h>
 
-File myFile;
+
+  const size_t BUFFER_SZ = 20;
+  
+  char buffer[BUFFER_SZ];
+  char woord[BUFFER_SZ];
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -39,41 +43,62 @@ void setup() {
   }
   Serial.println("initialization done.");
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  
 }
 
 void loop() {
   // nothing happens after setup
+  
+
+  wordFromFile(2, buffer);
+
+  for (int i = 0; i <= BUFFER_SZ; i++) {
+    woord[i] = buffer[i];
+  }
+
+  Serial.println(woord);
+
+   delay(1000);
 }
 
+void wordFromFile(int welkWoord, char *buffer) {
+int woordTeller = 0;
 
+File myFile;
+myFile = SD.open("test.txt");
+
+  if (myFile) {
+    
+    size_t pos = 0;    // current write position in buffer
+    int c;
+    
+    // While we can read a character:
+    while ((c = myFile.read()) != -1) {
+
+      // If we get a delimiter, then we have a complete element.
+      if (c == ',' || c == '\n') {
+        buffer[pos] = '\0';         // terminate the string
+      //Serial.println(buffer);     // write the word to serial monitor
+
+      // als het het woord is wat we willen, return
+      if (woordTeller == welkWoord) {
+        return;
+      }
+      woordTeller++;
+        pos = 0;                    // ready for next element
+      }
+
+      // Otherwise, store the character in the buffer.
+      else if (pos < BUFFER_SZ - 1) {
+        buffer[pos++] = c;
+      }
+    }
+    
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+}
