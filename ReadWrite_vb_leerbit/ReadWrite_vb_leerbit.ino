5828d396ae2b1b9cc22324707c213ed84b6582e9
kondrez <57896585+kondrez@users.noreply.github.com>
2020-01-17 14:57:55 +0100

    Update ReadWrite_vb_leerbit.ino

--- ReadWrite_vb_leerbit/ReadWrite_vb_leerbit.ino
+++ ReadWrite_vb_leerbit/ReadWrite_vb_leerbit.ino
@@ -27,6 +27,15 @@ const size_t BUFFER_SZ = 40;
 char woord[BUFFER_SZ];
 char username[BUFFER_SZ];
 
+char lines[15][200];
+
+char vraag[60];
+char antwoord1[60];
+char antwoord2[60];
+char antwoord3[60];
+char antwoord4[60];
+char juisteAntwoord[60];
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
@@ -43,53 +52,92 @@ void setup() {
   }
   Serial.println("initialization done.");
 
-
+  linesFromFile(); // read from sd card
+  int vrgNr = 5;
+  parseLine(vrgNr);
 }
 
+//  Serial.println(vraag);
 void loop() {
-  // nothing happens after setup
-
-  wordFromFile(2, username);
-  
-  Serial.println(username);
 
   delay(1000);
 }
 
-void wordFromFile(int welkWoord, char *buffer) {
+
+void parseLine(int vrgNr) {
+  int i = 0;
+  int mode = 1;
+  int cnt = 0;
+  
+  while (lines[vrgNr][i] != '\0') {
+//    Serial.print("c=");
+//    Serial.println(lines[vrgNr][i]);
+    if (lines[vrgNr][i] != ',') {
+      switch (mode) {
+        case 1: // vraag
+          vraag[cnt++] = lines[vrgNr][i];
+          break;
+        case 2: // antw 1
+          antwoord1[cnt++] = lines[vrgNr][i];
+          break;
+        case 3: // antw 2
+          antwoord2[cnt++] = lines[vrgNr][i];
+          break;
+        case 4: // antw 3
+          antwoord3[cnt++] = lines[vrgNr][i];
+          break;
+        case 5: // antw 4
+          antwoord4[cnt++] = lines[vrgNr][i];
+          break;
+        case 6: // antw 4
+          juisteAntwoord[cnt++] = lines[vrgNr][i];
+          break;
+      }
+    } else {
+      mode++;
+      cnt = 0;
+    }
+    i++;
+    //    Serial.print("i=");
+    //    Serial.println(i);
+  }
+  Serial.println(vraag);
+  Serial.println(antwoord1);
+  Serial.println(antwoord2);
+  Serial.println(antwoord3);
+  Serial.println(antwoord4);
+  Serial.println(juisteAntwoord);
+
+}
+
+void linesFromFile() {
   int woordTeller = 0;
+  int linenr = 0;
 
   File myFile;
   myFile = SD.open("test.txt");
 
   if (myFile) {
-
-    size_t pos = 0;    // current write position in buffer
+    int pos = 0;    // current write position in buffer
     int c;
 
     // While we can read a character:
     while ((c = myFile.read()) != -1) {
-
-      // als we een , of een enter inlezen hebben we een heel woord
-      if (c == ',' || c == '\n') {
-        buffer[pos] = '\0';         // terminate the string
-        
-        // als het het woord is wat we willen, return
-        if (woordTeller == welkWoord) {
-          return;
-        }
-
-        // anders verder naar het volgende woord
-        woordTeller++;
-        pos = 0;                    // ready for next element
-      }
-
-      // als het geen , of enter is door na het volgende char
-      else if (pos < BUFFER_SZ - 1) {
-        buffer[pos++] = c;
+      if (c != '\n') {
+        // zet in de array
+        lines[linenr][pos] = c;
+        //        Serial.print("linenr=");
+        //        Serial.print(linenr);
+        //        Serial.print(", pos=");
+        //        Serial.print(pos);
+        //        Serial.print(" : ");
+        //        Serial.println(lines[linenr][pos]);
+        pos++;
+      } else {
+        lines[linenr++][pos] = '\0';      // terminate the string
+        pos = 0;
       }
     }
-
     // close the file:
     myFile.close();
   } else {
